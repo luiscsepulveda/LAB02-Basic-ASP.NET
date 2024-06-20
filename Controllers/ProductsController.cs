594ec74555e19b8ca28b803e26a65ef270bd16a0
Luis C. Sepulveda <105004538+luiscsepulveda@users.noreply.github.com>
2024-06-20 14:25:57 -0400

    Add files via upload

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LabWebApp.Data;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabWebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.ToListAsync();
+            return View(products);
+        }
+    }
+}
